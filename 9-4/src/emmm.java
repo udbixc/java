1ffb01d5a386d5b2b00fded98f5c18b676420c86
zh <zh@zh.com>
2019-09-05 14:30:09 +0800

    测试

---
+++ 9-4/src/emmm.java
@@ -0,0 +1,19 @@
+public class emmm {
+    public static void main(String[] args) {
+        System.out.println("emmm,9-4，没有写");
+
+        test t1 = new test();
+        t1.name = "zh";
+        t1.t1();
+
+    }
+}
+
+class test{
+    int age;
+    String name;
+
+    void t1(){
+        System.out.println("不知道干嘛");
+    }
+}
